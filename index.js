66a2e21efd707c1b21a90a6f743c78ca7da99a2b
tomabadie <abadie.thomas@gmail.com>
2025-07-07 18:39:09 +0200

    Delete dead code - minor changes

--- index.js
+++ index.js
@@ -1,55 +1,46 @@
-//quiz ouverts niveau 1
+//Open quiz - level 1
 const htmlOne = document.querySelector("#html-n1");
 htmlOne.addEventListener("click", () => {
   localStorage.setItem("chosenTheme", "htmlBasics");
-  // document.location.href = "./quiz.html";
 });
 
 const cssOne = document.querySelector("#css-n1");
 cssOne.addEventListener("click", () => {
   localStorage.setItem("chosenTheme", "cssBasics");
-  // document.location.href = "./quiz.html";
 });
 
 const javascriptOne = document.querySelector("#javascript-n1");
 javascriptOne.addEventListener("click", () => {
   localStorage.setItem("chosenTheme", "jsBasics");
-  // document.location.href = "./quiz.html";
 });
 
 const gitOne = document.querySelector("#git-n1");
 gitOne.addEventListener("click", () => {
   localStorage.setItem("chosenTheme", "gitBasics");
-  // document.location.href = "./quiz.html";
 });
 
-//quiz ouverts niveau 2
-
+//Open quiz - level 2
 const htmlTwo = document.querySelector("#html-n2");
 htmlTwo.addEventListener("click", () => {
   localStorage.setItem("chosenTheme", "htmlIntermediate");
-  // document.location.href = "./quiz.html";
 });
 
 const cssTwo = document.querySelector("#css-n2");
 cssTwo.addEventListener("click", () => {
   localStorage.setItem("chosenTheme", "cssIntermediate");
-  // document.location.href = "./quiz.html";
 });
 
 const javascriptTwo = document.querySelector("#javascript-n2");
 javascriptTwo.addEventListener("click", () => {
   localStorage.setItem("chosenTheme", "jsIntermediate");
-  // document.location.href = "./quiz.html";
 });
 
 const gitTwo = document.querySelector("#git-n2");
 gitTwo.addEventListener("click", () => {
   localStorage.setItem("chosenTheme", "gitIntermediate");
-  // document.location.href = "./quiz.html";
 });
 
-//quiz fermés
+//Closed quiz
 const typescriptOne = document.querySelector("#typescript-n1");
 const pythonOne = document.querySelector("#python-n1");
 const phpOne = document.querySelector("#php-n1");
